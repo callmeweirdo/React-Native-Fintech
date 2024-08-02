22519dc1b164195a155bbcced405ab0a26bbca81
callmeweirdo <ogbandav102@gmail.com>
2024-08-02 21:44:25 +0100

    almost done with the home screen

--- fintech/app/_layout.tsx
+++ fintech/app/_layout.tsx
@@ -40,8 +40,6 @@ export default function RootLayout() {
   }
 
   return (
-    
-
     <TamaguiProvider config={tamaguiConfig}>
       <Stack>
         <Stack.Screen name='index' options={{ headerShown: false }} />
--- fintech/app/index.tsx
+++ fintech/app/index.tsx
@@ -1,34 +1,61 @@
-import { View, Text } from 'tamagui';
+import { View, Text, XGroup, Button } from 'tamagui';
 import { StyleSheet } from 'react-native';
 import { useAssets } from 'expo-asset';
-import { Video } from 'expo-av';
+import { ResizeMode, Video } from 'expo-av';
+import { Link } from 'expo-router';
 
 const index = () => {
     const [assets] = useAssets([require('@/assets/videos/intro.mp4')]);
   return (
     <View style={styles.container} >
-          {
+      {
         assets && <Video
           source={{ uri: assets[0].uri }}
           style={styles.video}
           isMuted
           isLooping
           shouldPlay
+          resizeMode={ResizeMode.COVER}
         />
-        }
+      }
+      <View style={{ marginTop: 80, padding: 20 }} >
+        <Text style={styles.header} >Ready to Change the way you Money ?</Text>
+      </View>
+      <View style={{ flexDirection: 'row', padding: 20, gap: 10 }} >
+        <Link href='/login' style={{backgroundColor: 'black'}} asChild>
+          <Button  href='/login' style={styles.button } color={'black'} >Login</Button>
+        </Link>
+        <Link href='/login' asChild>
+          <Button style={styles.button} >Sign Up</Button>
+        </Link>
+      </View>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center'
+    justifyContent: 'space-between',
+    
   },
   video: {
     width: '100%',
     height: '100%',
     position: 'absolute'
+  },
+  header: {
+    fontSize: 36,
+    fontWeight: '900',
+    textTransform: 'uppercase',
+    color: 'white'
+  },
+  button: {
+    fontSize: 22,
+    fontWeight: '500',
+    borderRadius: 50,
+    padding: 10,
+    width: '50%'
   }
 })
 
