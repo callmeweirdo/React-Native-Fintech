2179ffdde7ff623f689ef5afdab422d3904f43b8
callmeweirdo <ogbandav102@gmail.com>
2024-08-04 23:40:51 +0100

    trying the fix these errors i have been having on both web and mobile build. i works well now on the web and now remaining the mobile

--- fintech/app/_layout.tsx
+++ fintech/app/_layout.tsx
@@ -1,6 +1,5 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { Slot, Stack } from 'expo-router';
+import { Slot, Stack, useRouter } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
@@ -8,12 +7,14 @@ import 'react-native-reanimated';
 import { useColorScheme } from '@/hooks/useColorScheme';
 import { createTamagui, TamaguiProvider, Text, Theme } from 'tamagui';
 import { config } from '@tamagui/config/v3';
-
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { Ionicons } from '@expo/vector-icons';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
-export default function RootLayout() {
+const InitialLayout = () => {
+  const router = useRouter();
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
@@ -29,23 +30,42 @@ export default function RootLayout() {
     return null;
   }
 
-
   // you usually export this from a tamagui.config.ts file
-  const tamaguiConfig = createTamagui(config)
+  const tamaguiConfig = createTamagui(config);
 
   // TypeScript types across all Tamagui APIs
-  type Conf = typeof tamaguiConfig
+  type Conf = typeof tamaguiConfig;
   declare module '@tamagui/core' {
-    interface TamaguiCustomConfig extends Conf { }
+    interface TamaguiCustomConfig extends Conf {}
   }
 
   return (
     <TamaguiProvider config={tamaguiConfig}>
-      <Theme name={'yellow'} >
+      <Theme name={'yellow'}>
         <Stack>
-          <Stack.Screen name='signup' options={{ headerShown: false }} />
+          <Stack.Screen name="index" options={{ headerShown: false }} />
+          <Stack.Screen
+            name="signup"
+            options={{
+              title: '',
+              headerBackTitle: '',
+              headerShadowVisible: false,
+              headerStyle: { backgroundColor: '#fff' },
+              headerLeft: () => (
+                <TouchableOpacity onPress={ router.back } >
+                  <Ionicons name="arrow-back" size={28} color={'grey'} />
+                </TouchableOpacity>
+              ),
+            }}
+          />
         </Stack>
       </Theme>
     </TamaguiProvider>
   );
+};
+
+export default function RootLayout() {
+  return <>
+  <InitialLayout />;
+  </> 
 }
--- fintech/app/signup.tsx
+++ fintech/app/signup.tsx
@@ -1,78 +1,102 @@
-import {  StyleSheet, TextInput,TouchableOpacity } from 'react-native'
-import React, { useState } from 'react'
-import { H2, useTheme, View, Text, Button, } from 'tamagui'
+import {
+  KeyboardAvoidingView,
+  Platform,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+import React, { useState } from 'react';
+import { H2, useTheme, View, Button } from 'tamagui';
+import { Text } from 'react-native';
 import { Link } from 'expo-router';
 
-const signup = () => {
+const Signup = () => {
   const [countryCode, setCountryCode] = useState('+234');
-  const [phoneNumber, setPhoneNumber] = useState();
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const keyboardVerticalOffset = Platform.OS === 'ios' ? 90 : 100;
   const theme = useTheme();
 
-
-  const handleNumber = () => {
-
-  }
+  const handleNumber = (text) => {
+    setPhoneNumber(text);
+  };
 
   return (
-    <View style={styles.container} >
-      <H2 style={styles.H2} >Let's Get Started!</H2>
-      <Text style={{ marginBottom: 20 }} >Enter your phone number, Wel will send you a confirmation code there</Text>
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior="padding"
+      keyboardVerticalOffset={keyboardVerticalOffset}
+    >
+      <View style={styles.container}>
+        <H2 style={styles.H2}>Let's Get Started!</H2>
+        <Text style={{ marginBottom: 20 }}>
+          Enter your phone number, we will send you a confirmation code there.
+        </Text>
 
-      <View style={styles.inputContainer}>
-        <TextInput
-          placeholder='Country Code'
-          value={countryCode}
-          placeholderTextColor={theme.yellow10?.get()}
-          style={[styles.input, { width: '25%' }]}
-          keyboardType='numeric'
-        />
-        <TextInput
-          placeholder='Phone Number'
-          placeholderTextColor={theme.yellow10?.get()}
-          style={[styles.input, { flex: 1, width: '85%' }]}
-          keyboardType='numeric'
-          value={phoneNumber}
-          onChange={handleNumber}
-        />
-      </View>
-      <Link href={'/login'} asChild replace>
-        <TouchableOpacity style={{ marginTop: 20 }} >
-          <Text color={theme.blue10?.get()} >Already have an Account ?</Text>
-        </TouchableOpacity>
-      </Link>
-
-      <View style={{ flex: 1 }} />
-
-      <Link href='/signup' asChild>
-          <Button style={{ marginTop: 20, fontWeight: '500' }} >Sign Up</Button>
+        <View style={styles.inputContainer}>
+          <TextInput
+            placeholder="Country Code"
+            value={countryCode}
+            placeholderTextColor={theme.yellow10?.get()}
+            style={[styles.input, { width: '25%' }]}
+            keyboardType="numeric"
+          />
+          <TextInput
+            placeholder="Phone Number"
+            placeholderTextColor={theme.yellow10?.get()}
+            style={[styles.input, { flex: 1, width: '85%' }]}
+            keyboardType="numeric"
+            value={phoneNumber}
+            onChangeText={handleNumber}
+          />
+        </View>
+        <Link href={'/login'} asChild replace>
+          <TouchableOpacity style={{ marginTop: 20 }}>
+            <Text color={theme.blue10?.get()}>Already have an Account?</Text>
+          </TouchableOpacity>
         </Link>
-    </View>
+
+        <View style={{ flex: 1 }} />
+
+        <Button
+          style={[
+            styles.button,
+            { backgroundColor: phoneNumber !== '' ? theme.yellow10?.get() : 'grey' },
+          ]}
+        >
+          Sign Up
+        </Button>
+      </View>
+    </KeyboardAvoidingView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     marginVertical: 10,
     flexDirection: 'column',
-    padding: 20
+    padding: 20,
   },
   H2: {
     fontWeight: '500',
     textAlign: 'center',
     padding: 20,
-    // marginBottom: 20
   },
   inputContainer: {
     flexDirection: 'row',
     gap: 10,
-    width: '100%'
+    width: '100%',
   },
   input: {
     padding: 15,
     borderRadius: 16,
     fontSize: 20,
-    backgroundColor: 'grey'
-  }
-})
+    backgroundColor: 'grey',
+  },
+  button: {
+    marginTop: 20,
+    fontWeight: 'bold',
+    borderRadius: 30,
+  },
+});
 
-export default signup
+export default Signup;
